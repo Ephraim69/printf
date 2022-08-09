ad70ce3bceaf7e7731b7537aa936eb6b8b988dab
Ephraim69 <Ephraimigbinosa@gmail.com>
2022-08-09 20:26:10 +0000

    updated _printf function

--- _printf.c
+++ _printf.c
@@ -3,9 +3,9 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - a function that prints formouted output to stdout
+ * @format: the string that contains any format specifier
+ * Return: returns the number of printed characters
  */
 int _printf(const char *format, ...)
 {
@@ -26,7 +26,6 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
